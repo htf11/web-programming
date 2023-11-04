85b19a3ba0c4649e27a8c4c2f6b651ece30dee17
Alex <alex.inck2000@yandex.ru>
2023-11-04 13:03:43 +0300

    fix: lab4 done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,23 @@
-const allImages = new Array(
-  "1.jpg",
-  "2.jpg",
-  "3.jpg",
-  "4.jpg",
-  "5.jpg",
-  "6.jpg"
-);
-const dirName = "./assets/img/";
-
-function load(){
-  let img = document.querySelector('img');
-  img.src = dirName + allImages[0];
+function checkform() {
+    const elem = document.querySelectorAll('.checkout');
+    const validateName = new RegExp('[A-Z]([a-z]){1,}');
+    const validateNumber = new RegExp('(\\+7)([0-9]){10}');
+    elem.forEach(n => {
+        if (n.value == '') {
+            n.setCustomValidity("Field is empty");
+        } else n.setCustomValidity("");
+    });
+    if (!validateName.test(elem[0].value)) {
+        elem[0].setCustomValidity("Incorrect name format");
+    } else elem[0].setCustomValidity("");
+    if (!validateNumber.test(elem[7].value)) {
+        elem[7].setCustomValidity("Incorrect number format");
+    } else elem[7].setCustomValidity("");
+    elem[3].addEventListener("input", function (event) {
+        if (elem[3].validity.typeMismatch) {
+            elem[3].setCustomValidity("It's not e-mail address!");
+        } else {
+            elem[3].setCustomValidity("");
+        }
+    });
 }
-
-function change_img(dir) {
-  let img = document.querySelector('img');
-  let item = img.src.split('/');
-  let imgName = item[item.length - 1];
-  let index = allImages.indexOf(imgName);
-  let newIndex = index + dir;
-  if (newIndex > allImages.length - 1) {
-    newIndex = 0;
-  }
-  if (newIndex < 0) {
-    newIndex = allImages.length - 1;
-  }
-  console.log(newIndex);
-  img.src = dirName + allImages[newIndex];
-}
-
-window.onload = load();
