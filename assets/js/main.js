5dd84ac55ddd6086d6e2caa770024d194201da61
Alex <alex.inck2000@yandex.ru>
2023-12-04 14:46:00 +0300

    feat: end working on lab6

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,67 @@
+const form = document.querySelector("#form");
+const elem = document.querySelectorAll(".checkout");
+const email = document.querySelector("#email");
+const phone = document.querySelector("#phone");
+
 function checkform() {
-    const elem = document.querySelectorAll('.checkout');
-    const validateName = new RegExp('[A-Z]([a-z]){1,}');
-    const validateNumber = new RegExp('(\\+7)([0-9]){10}');
-    elem.forEach(n => {
-        if (n.value == '') {
-            n.setCustomValidity("Field is empty");
-        } else n.setCustomValidity("");
-    });
-    if (!validateName.test(elem[0].value)) {
-        elem[0].setCustomValidity("Incorrect name format");
-    } else elem[0].setCustomValidity("");
-    if (!validateNumber.test(elem[7].value)) {
-        elem[7].setCustomValidity("Incorrect number format");
-    } else elem[7].setCustomValidity("");
-    elem[3].addEventListener("input", function (event) {
-        if (elem[3].validity.typeMismatch) {
-            elem[3].setCustomValidity("It's not e-mail address!");
-        } else {
-            elem[3].setCustomValidity("");
-        }
-    });
+  let valid = true;
+  removeValidation();
+  for (let i = 0; i < elem.length; i++) {
+    if (elem[i].value.trim() === "") {
+      valid = false;
+      elem[i].setCustomValidity("Field is empty");
+    }
+  }
+  if (validateEmail() === false) {
+    valid = false;
+  }
+  if (validatePhone() === false) {
+    valid = false;
+  }
+  if (valid) {
+    let data = {
+      firstName: elem[0].value,
+      lastName: elem[1].value,
+      company: elem[2].value,
+      email: elem[3].value,
+      country: document.getElementById("country").value,
+      address: elem[4].value,
+      town: elem[5].value,
+      zip: elem[6].value,
+      phone: elem[7].value,
+      review: document.getElementById("review").value
+    }
+    fetch("/server", { 
+      method: "POST", 
+      headers: { "Accept": "application/json", "Content-Type": "application/json" },
+      body: JSON.stringify(data)
+    })
+    .finally(() => alert("Данные успешно отправлены"))
+    .then(response => response.text())
+    .then(obj => console.log(obj))
+  }
+}
+
+function removeValidation() {
+  for (let i = 0; i < elem.length; i++) {
+    elem[i].setCustomValidity("");
+  }
+}
+
+function validateEmail() {
+  const validateEmail = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+  let mail = email.value;
+  if (validateEmail.test(mail) == false) {
+    email.setCustomValidity("Incorrect e-mail address");
+    return false;
+  }
+}
+
+function validatePhone() {
+  const validateNumber = new RegExp("(\\+7)([0-9]){10}");
+  let number = phone.value;
+  if (validateNumber.test(number) == false) {
+    phone.setCustomValidity("Incorrect phone format");
+    return false;
+  }
 }
